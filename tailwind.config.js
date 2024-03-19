5f7d6dbf362adad58deb2075be83209305762b7b
kengiiGMs <kaikiaoto22@gmail.com>
2024-03-19 12:26:20 -0300

    5 - Adicionando uma fonte ao Projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    fontFamily: {
+      'sans': ['Lato', 'sans-serif']
+    },
     extend: {
       backgroundImage: {
         "home": "url('/assets/banner.png')"
