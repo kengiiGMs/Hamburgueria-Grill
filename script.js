2d06886b8f2f3a63e0f74159f48fb28224ef8b92
kengiiGMs <kaikiaoto22@gmail.com>
2024-03-19 22:24:40 -0300

    6.1 - Adicionando e Exibindo Itens no Carrinho

--- script.js
+++ script.js
@@ -1,19 +1,93 @@
-const cartBtn = document.getElementById("cart-btn")
-const cartModal = document.getElementById("cart-modal")
-const closeModalBtn = document.getElementById("close-modal-btn")
+const cartBtn = document.getElementById("cart-btn");
+const cartModal = document.getElementById("cart-modal");
+const closeModalBtn = document.getElementById("close-modal-btn");
+
+const menu = document.getElementById("menu");
+let cart = []; //Array para armazenar os itens
+
+const cartItemsContainer = document.getElementById("cart-items");
+const cartTotal = document.getElementById("cart-total");
+const cartCounter = document.getElementById("cart-count");
 
 /* Modal */
 cartBtn.addEventListener("click", function () {
-    cartModal.style.display = "flex"
-})
+    updateCartModal();
+    cartModal.style.display = "flex";
+});
 
 cartModal.addEventListener("click", function (event) {
     if (event.target === cartModal) {
-        cartModal.style.display = "none"
+        cartModal.style.display = "none";
     }
 })
 
 closeModalBtn.addEventListener("click", function () {
-    cartModal.style.display = "none"
+    cartModal.style.display = "none";
 })
-/* Modal */
+
+/* Coletando informações dos itens */
+menu.addEventListener("click", function (event) {
+
+    let parentButton = event.target.closest(".add-to-cart-btn");
+
+    if (parentButton) {
+        const name = parentButton.getAttribute("data-name");
+        const price = parseFloat(parentButton.getAttribute("data-price"));
+        addItem(name, price);
+    }
+})
+
+/* Função para adicionar ao carrinho */
+function addItem(name, price) {
+    const existingItem = cart.find(item => item.name === name)
+
+    if (existingItem) {
+        /* Se o Item já existir, aumenta apenas a quantidade + 1 */
+        existingItem.quantity += 1;
+    } else {
+        cart.push({
+            name,
+            price,
+            quantity: 1
+        })
+    }
+
+    updateCartModal();
+}
+
+/* Atualiza o carrinho */
+function updateCartModal() {
+    cartItemsContainer.innerHTML = "";
+    let total = 0;
+
+    cart.forEach(item => {
+        const cartItemElement = document.createElement("div");
+        cartItemElement.classList.add("flex", "justify-between", "mb-4", "flex-col");
+
+        cartItemElement.innerHTML = `
+            <div class="flex items-center justify-between">
+                <div>
+                    <p class="font-bold">${item.name}</p>
+                    <p>Qtd: ${item.quantity}</p>
+                    <p class="font-medium mt-2">${item.price.toFixed(2)}</p>
+                </div>
+
+                <div>
+                    <button>
+                        Remover
+                    </button>
+                </div>
+            </div>
+        `
+
+        total += item.price * item.quantity;
+        cartItemsContainer.appendChild(cartItemElement);
+    })
+
+    cartTotal.textContent = total.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    });
+
+    cartCounter.innerHTML = cart.length;
+}
